6ee3c9dbe57c4a9277a833ecf38cd63495eab618
Jakub Rychter <jakubrychter00@gmail.com>
2025-04-02 16:25:03 +0200

    Dodanie dzialjacego algorytmu do dodania wybor opcji

--- pythonProject/gaussJordanElimination.py
+++ pythonProject/gaussJordanElimination.py
@@ -1,11 +1,17 @@
+from decimal import Decimal, getcontext
+
+
 def jordanElimination(A, b, n):
 
-    Ab = [A[i] + [b[i]] for i in range(n)]
+    getcontext().prec = 15
+
+
+    Ab = [[Decimal(A[i][j]) for j in range(n)] + [Decimal(b[i])] for i in range(n)]
 
     for i in range(n):
 
         max_row = max(range(i, n), key=lambda r: abs(Ab[r][i]))
-        if Ab[max_row][i] == 0:
+        if abs(Ab[max_row][i]) < Decimal("1e-12"):
             continue
 
         Ab[i], Ab[max_row] = Ab[max_row], Ab[i]
@@ -19,10 +25,10 @@ def jordanElimination(A, b, n):
                 Ab[j] = [Ab[j][k] - factor * Ab[i][k] for k in range(n + 1)]
 
     for i in range(n):
-        if all(Ab[i][j] == 0 for j in range(n)):
-            if Ab[i][n] != 0:
+        if all(abs(Ab[i][j]) < Decimal("1e-12") for j in range(n)):
+            if abs(Ab[i][n]) > Decimal("1e-12"):
                 return "Układ sprzeczny"
             return "Układ nieoznaczony"
 
 
-    return [Ab[i][n] for i in range(n)]
+    return [round(float(Ab[i][n]), 6) for i in range(n)]
--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,6 +3,10 @@ from readMatrixFromFile import read_matrices_file
 
 filename = "macierze.txt"
 matrices= read_matrices_file(filename)
+
+# for matrix in matrices:
+#     print(matrix)
+
 for A, b in matrices:
     n = len(A)
     wynik = jordanElimination(A, b, n)
